774cd96e5b89485a8c267fe57f962f1baf43c842
Atakan Can <atakancan2000@gmail.com>
2025-12-15 17:23:31 +0300

    Fix style issues, should get a 'Good Style' now :pray:

--- today.py
+++ today.py
@@ -1,4 +1,7 @@
+# İsim Fonksiyonu
 def my_name_is():
+    #ismimi gösteren denklem
     return "atakancansuit"
 def my_buddy_is():
+    #arkadaşımın ismini gösteren denklem
     return "atakancansuit"
